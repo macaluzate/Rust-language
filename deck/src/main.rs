d925b1663c8f5a4ceffa7afa8a94c0406a0db7f3
Macaluzate <mcanavera@unal.edu.co>
2025-03-15 12:56:35 -0500

    Agregando el metodo deal e implementandolo correctamente

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -25,11 +25,21 @@ impl Deck {
         let mut rng = thread_rng();
         self.cards.shuffle(&mut rng);
     }
+
+    fn deal(&mut self, num_cards: usize ) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+
+
+    }
 }
 
 fn main() {
     let mut deck = Deck::new();
-    deck.shuffle();
+    //deck.shuffle();
+    println!("Here is my deck {:#?}", deck);
+    let cards = deck.deal(3);
 
+    
+    println!("Here is my hand {:#?}", cards);
     println!("Here is my deck {:#?}", deck);
 }
